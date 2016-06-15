04bd146436b95a1c32bab931d4bc266c6ca5ff79
jackzhou <fiattt@msn.com>
2016-06-15 13:37:18 +0800

    添加推送开关  enable、disable、isEnabled方法

--- UmengPush.js
+++ UmengPush.js
@@ -1,11 +1,11 @@
 'use strict';
 
 import React, {
-  NativeModules,
-  DeviceEventEmitter, //android
-  NativeAppEventEmitter, //ios
-  Platform,
-  AppState,
+    NativeModules,
+    DeviceEventEmitter, //android
+    NativeAppEventEmitter, //ios
+    Platform,
+    AppState,
 } from 'react-native';
 
 const UmengPushModule = NativeModules.UmengPush;
@@ -14,37 +14,49 @@ var receiveMessageSubscript, openMessageSubscription;
 
 var UmengPush = {
 
-  getDeviceToken(handler: Function) {
-    UmengPushModule.getDeviceToken(handler);
-  },
+    enable(){
+        UmengPushModule.enable();
+    },
 
-  didReceiveMessage(handler: Function) {
-    receiveMessageSubscript = this.addEventListener(UmengPushModule.DidReceiveMessage, message => {
-      //处于后台时，拦截收到的消息
-      if(AppState.currentState === 'background') {
-        return;
-      }
-      handler(message);
-    });
-  },
+    disable(){
+        UmengPushModule.disable();
+    },
 
-  didOpenMessage(handler: Function) {
-    openMessageSubscription = this.addEventListener(UmengPushModule.DidOpenMessage, handler);
-  },
+    isEnabled(handler:Function) {
+        UmengPushModule.isEnabled(handler);
+    },
 
-  addEventListener(eventName: string, handler: Function) {
-    if(Platform.OS === 'android') {
-      return DeviceEventEmitter.addListener(eventName, (event) => {
-        handler(event);
-      });
-    }
-    else {
-      return NativeAppEventEmitter.addListener(
-        eventName, (userInfo) => {
-          handler(userInfo);
+    getDeviceToken(handler:Function) {
+        UmengPushModule.getDeviceToken(handler);
+    },
+
+    didReceiveMessage(handler:Function) {
+        receiveMessageSubscript = this.addEventListener(UmengPushModule.DidReceiveMessage, message => {
+            //处于后台时，拦截收到的消息
+            if (AppState.currentState === 'background') {
+                return;
+            }
+            handler(message);
         });
-    }
-  },
+    },
+
+    didOpenMessage(handler:Function) {
+        openMessageSubscription = this.addEventListener(UmengPushModule.DidOpenMessage, handler);
+    },
+
+    addEventListener(eventName:string, handler:Function) {
+        if (Platform.OS === 'android') {
+            return DeviceEventEmitter.addListener(eventName, (event) => {
+                handler(event);
+            });
+        }
+        else {
+            return NativeAppEventEmitter.addListener(
+                eventName, (userInfo) => {
+                    handler(userInfo);
+                });
+        }
+    },
 };
 
 module.exports = UmengPush;
